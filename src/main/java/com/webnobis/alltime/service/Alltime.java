4519ab5930468febe17577aea592844150ec7f8b
steffen <github@webnobis.com>
2017-08-23 13:20:19 +0200

    temporary set service to check startAZ button action

--- src/main/java/com/webnobis/alltime/service/Alltime.java
+++ src/main/java/com/webnobis/alltime/service/Alltime.java
@@ -1,19 +1,94 @@
 package com.webnobis.alltime.service;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+
+import com.webnobis.alltime.config.Config;
+import com.webnobis.alltime.model.Entry;
+import com.webnobis.alltime.model.EntryType;
+import com.webnobis.alltime.persistence.EntryStore;
+import com.webnobis.alltime.persistence.FileEntryStore;
 import com.webnobis.alltime.view.BookingDialog;
 
 import javafx.application.Application;
+import javafx.scene.control.Dialog;
 import javafx.stage.Stage;
 
 public class Alltime extends Application {
+	
+	private static final String CONFIG_FILE = "config.properties";
+	
+	private EntryService service;
 
 	public static void main(String[] args) {
 		Application.launch(Alltime.class, args);
 	}
 
 	@Override
+	public void init() throws Exception {
+		super.init();
+		
+		Path configFile = Paths.get(this.getClass().getClassLoader().getResource(CONFIG_FILE).getPath());
+		Config config = new Config(configFile);
+		service = createService(config, createStore(config));
+	}
+	
+	private static EntryStore createStore(Config config) {
+		return new FileEntryStore(config.getFileStoreRootPath(), s -> new Entry(){
+
+			@Override
+			public LocalDate getDay() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public EntryType getType() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public LocalTime getStart() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Duration getTimeAssets() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Map<String, Duration> getItems() {
+				// TODO Auto-generated method stub
+				return null;
+			}}, e -> "");
+	}
+
+	private static EntryService createService(Config config, EntryStore store) {
+		return new EntryService(() -> LocalTime.now(), 
+				config.getMaxCountOfDays(), 
+				config.getExpectedTimes(), 
+				config.getIdleTimes(), 
+				store);
+	}
+
+	@Override
 	public void start(Stage primaryStage) throws Exception {
-		new BookingDialog(null);
+		
+/*		Scene bookingScene = new Scene(null);
+		Stage bookingStage = new Stage();
+		bookingStage.setScene(bookingScene);
+		bookingStage.centerOnScreen();
+	*/	
+		
+		Dialog<Entry> bookingDialog = new BookingDialog(service);
 		
 	}
 
--- src/main/java/com/webnobis/alltime/view/BookingDialog.java
+++ src/main/java/com/webnobis/alltime/view/BookingDialog.java
@@ -1,11 +1,15 @@
 package com.webnobis.alltime.view;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import com.webnobis.alltime.model.Entry;
 import com.webnobis.alltime.model.EntryType;
 import com.webnobis.alltime.service.EntryService;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Dialog;
 import javafx.scene.layout.GridPane;
@@ -14,19 +18,39 @@ public class BookingDialog extends Dialog<Entry> {
 	
 	private final EntryService service;
 	
+	private final ValueField<LocalDate> day;
+	
+	private final ValueField<LocalTime> startTime;
+	
 	private final ComboBox<EntryType> typeBox;
+	
+	private final Button startAZ;
 
 	public BookingDialog(EntryService service) {
-		super();
 		this.service = service;
 		
+		day = new ValueField<>(DayTransformer::toDay, DayTransformer::toText);
+		startTime = new ValueField<>(TimeTransformer::toTime, TimeTransformer::toText);
 		typeBox = new ComboBox<>(FXCollections.observableArrayList(EntryType.values()));
+		
+		startAZ = new Button("Start AZ");
+		startAZ.setOnAction(this::startAZ);
+		
 		GridPane pane = new GridPane();
-		pane.add(typeBox, 0,0);
+		pane.add(day, 0,0);
+		pane.add(startTime, 0,1);
+		pane.add(typeBox, 0,2);
+		pane.add(startAZ, 0,3);
 		
 		super.getDialogPane().setContent(pane);
+		//super.onShowingProperty().addListener(observable -> observable.addListener(e -> e.));
 		super.show();
 	}
 
+	private void startAZ(ActionEvent event) {
+		super.setResult(service.startAZ(day.getValue(), startTime.getValue()));
+		
+		System.out.println(super.getResult());
+	}
 
 }
