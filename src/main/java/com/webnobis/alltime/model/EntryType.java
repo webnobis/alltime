7209e8bec71af6358b1cbde6a3dc101397ea0c12
Steffen Nobis <github@webnobis.com>
2017-10-04 09:05:22 +0200

    entry type extended

--- src/main/java/com/webnobis/alltime/model/EntryType.java
+++ src/main/java/com/webnobis/alltime/model/EntryType.java
@@ -1,23 +1,24 @@
 package com.webnobis.alltime.model;
 
 public enum EntryType {
-	
-	AZ("Arbeitszeit"),
-	GT("Gleittag"),
-	UR("Urlaub"),
-	KR("krank"),
-	SM("Seminar"),
-	SO("Sondertag"),
-	WE("Wochenende");
-	
-	private final String description;
 
-	private EntryType(String description) {
-		this.description = description;
-	}
+    AZ("Arbeitszeit"),
+    GT("Gleittag"),
+    UR("Urlaub"),
+    KR("krank"),
+    SM("Seminar"),
+    SO("Sondertag"),
+    WE("Wochenende"),
+    FT("Feiertag");
 
-	public String getDescription() {
-		return description;
-	}
-	
+    private final String description;
+
+    private EntryType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
