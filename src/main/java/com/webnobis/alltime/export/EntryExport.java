3327a1b2d6fe0a6852c820124835631ddc6b1ece
steffen <github@webnobis.com>
2017-10-08 22:10:31 +0200

    export interface defined

---
+++ src/main/java/com/webnobis/alltime/export/EntryExport.java
@@ -0,0 +1,21 @@
+package com.webnobis.alltime.export;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import com.webnobis.alltime.model.Entry;
+
+public interface EntryExport {
+
+	List<Entry> exportRange(LocalDate fromDay, LocalDate untilDay);
+
+	default List<Entry> exportMonth(YearMonth month) {
+		return Optional.ofNullable(month)
+				.map(m -> exportRange(m.atDay(1), m.atEndOfMonth()))
+				.orElseGet(Collections::emptyList);
+	}
+
+}
