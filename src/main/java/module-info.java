840ebcd0d5b710d94e5c9b6fdd3dad358bf117cc
steffen <github@webnobis.com>
2018-09-13 22:31:33 +0200

    modules since java 9 try to configure - add-modules not work, because classpath or module call in shell script is not successful, missing modularization of iText-pdf

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.webnobis.alltime.Alltime {
+	
+	requires java.base;
+	
+	requires org.slf4j;
+	
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.swing;
+	
+	exports com.webnobis.alltime;
+	
+}
