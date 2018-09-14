cca1f3ec0d7c9fd745ebb2703f6fc4f948790564
steffen <github@webnobis.com>
2018-09-14 21:42:01 +0200

    surefire doesn't work with modules on java 11; mvn clean test works; next: fix artefact buildings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,13 @@ module com.webnobis.alltime.Alltime {
 	
 	requires org.slf4j;
 	
+	/*
+	 * iTextPdf is not ready for modularization (14.09.2018), therefore the name of the artifact
+	 */
+	requires kernel;
+	requires io;
+	requires layout;
+	
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.graphics;
